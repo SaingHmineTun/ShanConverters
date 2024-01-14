ea8231a5f973a947a7d08d076212592c47884ee3
Mein <saisainghminetun@gmail.com>
2024-01-14 11:58:24 +0630

    Implemented Shan Word Sorting version 1

--- src/main/java/it/saimao/shan_converter/word_sordings/ShanWordSorting.java
+++ src/main/java/it/saimao/shan_converter/word_sordings/ShanWordSorting.java
@@ -2,6 +2,8 @@ package it.saimao.shan_converter.word_sordings;
 
 import it.saimao.shan_converter.breakers.ShanRuleBasedSyllableSegmentation;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Stream;
@@ -15,61 +17,96 @@ public class ShanWordSorting implements Comparator<String> {
             "မ", "ယ", "ရ", "လ", "ဝ", "ႁ", "ဢ"
     };
 
-    static String a = "ဢ";
     static String asat = "\u103A";
 
 
     // V - 20 - 29
     static String[] vowels = {
-        "ဢႃ", "ဢီ", "ဢေ", "ဢႄ", "ဢူ", "ဢူဝ်", "ဢေႃ", "ဢိုဝ်", "ဢိူဝ်", "ဢႂ်"
+            "ဢႃ", "ဢီ", "ဢေ", "ဢႄ", "ဢူ", "ဢူဝ်", "ဢေႃ", "ဢိုဝ်", "ဢိူဝ်", "ဢႂ်"
     };
 
     static String[] tones = {"ႇ", "ႈ", "း", "ႉ", "့", "ႊ"};
 
     // D - 30 - 37
     static String[] consonantsCombination = {
-            "ၵ","င","ၺ", "တ","ၼ", "ပ", "မ","ဝ"
+            "ၵ", "င", "ၺ", "တ", "ၼ", "ပ", "မ", "ဝ"
     };
 
     // ဢၢပ် ၊ ဢိပ် ၊ ဢဵပ် ၊ ဢႅပ် ၊ ဢုပ် ၊ ဢူပ် ၊ ဢွပ် ၊ ဢိုပ် ၊ ဢိူပ်
     static String[] deVowelizers = {
-            "\u1062", "\u102D", "\u1035", "\u1085", "\u102F", "\u1030", "\u103D", "ို", "ိူ"
+            "", "\u1062", "\u102D", "\u1035", "\u1085", "\u102F", "\u1030", "\u103D", "\u102D\u102F", "\u102D\u1030"
     };
 
     // ၵျ တြ ၵႂ ၵွ
     // CC - 38 - 41
-    static String[] medians = {"ျ", "ြ", "ႂ်", "ႂ", "ွ"};
+    static String[] medians = {"ျ", "ြ", "ႂ်", "ႂ"};
 
-
-    static List<String> weights = Stream.of(consonants, medians, vowels, tones, consonantsCombination, deVowelizers).flatMap(Stream::of).toList();
+    static List<String> tones1;
+    static List<String> tones2;
 
 
     public static String compareWordString(String w1, String w2) {
         int compareWordInt = compareWordInt(w1, w2);
-        if (compareWordInt == 1) return w1;
-        else if (compareWordInt == -1) return w2;
+        if (compareWordInt == 1) return w2;
+        else if (compareWordInt == -1) return w1;
         else return "Same Word";
     }
 
     public static int compareWordInt(String w1, String w2) {
         if (w1.equals(w2)) return 0;
+        tones1 = new ArrayList<>();
+        tones2 = new ArrayList<>();
         w1 = w1.replaceAll("ႆ", "ၺ်");
         w2 = w2.replaceAll("ႆ", "ၺ်");
         int maxWeight = 0;
         String[] ss1 = ShanRuleBasedSyllableSegmentation.segmentAsArray(w1);
         String[] ss2 = ShanRuleBasedSyllableSegmentation.segmentAsArray(w2);
         int lastIndex = Math.min(ss1.length, ss2.length);
-        for (int i = 0; i < lastIndex; i ++) {
+        for (int i = 0; i < lastIndex; i++) {
             String s1 = ss1[i];
             String s2 = ss2[i];
             maxWeight = compareSyllable(s1, s2);
+            if (maxWeight != 0) break;
+//            if (maxWeight == 0) {
+//                if (i == lastIndex - 1) {
+//                    // Reach the end of a syllable
+//                    maxWeight = compareValue(w1.length(), w2.length());
+//                    break;
+//                }
+//            } else
+//                break;
+        }
+        if (maxWeight == 0) {
+            maxWeight = compareValue(w1.length(), w2.length());
             if (maxWeight == 0) {
-                if (i == lastIndex - 1) {
+                maxWeight = compareTone();
+                if (maxWeight == 0) {
                     // Reach the end of a syllable
                     return compareValue(w1.length(), w2.length());
                 }
             }
-            if (maxWeight != 0) break;
+        }
+
+        // Every word has the same weight
+        // Check tones then
+        // First check tones length, which one has more tones mean
+        // it has to come after!
+
+        return maxWeight;
+    }
+
+    private static int compareTone() {
+        int maxWeight = 0;
+        if (!tones1.isEmpty() && !tones2.isEmpty()) {
+            maxWeight = compareValue(tones1.size(), tones2.size());
+            if (maxWeight == 0) {
+                for (int x = 0; x < tones1.size() && x < tones2.size(); x++) {
+                    int t1 = getToneInt(tones1.get(x));
+                    int t2 = getToneInt(tones2.get(x));
+                    maxWeight = compareValue(t1, t2);
+                    if (maxWeight != 0) break;
+                }
+            }
         }
         return maxWeight;
     }
@@ -106,9 +143,7 @@ public class ShanWordSorting implements Comparator<String> {
             result = compareValue(d1, d2);
             // ၶၢတ်ႇ လႄႈ ၶၢတ်ႈ - ၶၢတ်ႇတေလႆႈမႃးဢွၼ်တၢင်း
             if ((d1 != -1 && d2 != -1) && result == 0) {
-                int t1 = getToneWeight(s1);
-                int t2 = getToneWeight(s2);
-                result = compareValue(t1, t2);
+                addTones(s1, s2);
             }
             return result;
         }
@@ -116,7 +151,7 @@ public class ShanWordSorting implements Comparator<String> {
     }
 
     private static int getDevowelizationWeight(String s) {
-        for (int i = 0, x = deVowelizers.length - 1; x >= 0; i++, x --) {
+        for (int i = 0, x = deVowelizers.length - 1; x >= 0; i++, x--) {
             String devowlizer = deVowelizers[x];
             if (s.contains(devowlizer)) return x;
         }
@@ -126,9 +161,9 @@ public class ShanWordSorting implements Comparator<String> {
 
     private static int getConsonantCombinationWeight(String s1) {
         int i = 0;
-        for (String cc: consonantsCombination) {
+        for (String cc : consonantsCombination) {
             if (s1.contains(cc + asat)) return i;
-            i ++;
+            i++;
         }
         return -1;
     }
@@ -138,38 +173,68 @@ public class ShanWordSorting implements Comparator<String> {
         int v2 = getVowelWeight(s2);
         int result = compareValue(v1, v2);
         if ((v1 != -1 && v2 != -1) && result == 0) {
-            int t1 = getToneWeight(s1);
-            int t2 = getToneWeight(s2);
-            return compareValue(t1, t2);
+            addTones(s1, s2);
         }
         return result;
     }
 
-    private static int getToneWeight(String s) {
-        for (String tone: tones) {
+    private static void addTones(String s1, String s2) {
+        String t1 = getToneWeight(s1);
+        if (t1 != null) tones1.add(t1);
+        String t2 = getToneWeight(s2);
+        if (t2 != null) tones2.add(t2);
+    }
+
+    private static String getToneWeight(String s) {
+        for (String tone : tones) {
             if (s.contains(tone)) {
-                return weights.indexOf(tone);
+                return tone;
             }
+        }
+        return null;
+    }
+
+    private static int getToneInt(String s) {
+        int i = 0;
+        for (String tone : tones) {
+            if (s.contains(tone)) {
+                return i;
+            }
+            i++;
         }
         return -1;
     }
 
     private static int getMedianWeight(String s1) {
-        for (String median: medians) {
+        int i = 0;
+        for (String median : medians) {
             if (s1.contains(median)) {
-                return weights.indexOf(median);
+                return i;
             }
+            i++;
         }
         return -1;
     }
 
+    private static String u = "ူ";
 
     private static int getVowelWeight(String syllable) {
 
-        for (int i = vowels.length - 1; i >= 0; i --) {
+        for (int i = vowels.length - 1; i >= 0; i--) {
             String vowel = vowels[i].substring(1);
             if (syllable.contains(vowel)) {
-                return weights.indexOf(a + vowel);
+                /*
+                ဢူ ၼႆႉ မီးလွင်ႈယူပ်ႈယွမ်းဝႆႉ
+                ၵွပ်ႈပိူဝ် ဢူမ် ၊ ဢိူမ် ၸိူဝ်းၼႆႉ ၸၢင်ႊၶဝ်ႈမႃးလႆႈတီႈၼႆႈဢိူဝ်ႈ
+                ၵွပ်ႈၼႆလႆႈၸႅတ်ႈဝႆႉဝႃႈ
+                တူဝ်လိၵ်ႈႁဝ်းၼႆႉ တေဢမ်ႇလႆႈၼမ်သေ3တူဝ်ၼႆယဝ်ႉ
+                ၵွပ်ႈပိူဝ် ဢူ၊ ဢူး ၼႆႉမီးလႆႈသၢမ်တူဝ်ၵူၺ်းသေ
+                ဢူမ်၊ ဢိူမ်း ၸိူဝ်းၼႆႉ ၼမ်သေ သၢမ်တူဝ်ဝႆႉယဝ်ႉ။
+                 */
+                if (vowel.equals(u)) {
+                    if (syllable.length() <= 3)
+                        return i;
+                } else return i;
             }
         }
         return -1;
@@ -177,13 +242,20 @@ public class ShanWordSorting implements Comparator<String> {
 
     private static int getConsonantWeight(String s) {
         String c = String.valueOf(s.charAt(0));
-        return weights.indexOf(c);
+        return Arrays.asList(consonants).indexOf(c);
     }
 
 
     public static int compareValue(int v1, int v2) {
-        return Integer.compare(v2, v1);
-//        return Integer.compare(v2, v1);
+        if (v1 >= 0 && v2 >= 0) {
+            if (v1 > v2) return 1;
+            else if (v2 > v1) return -1;
+            else return 0;
+        } else if (v1 < 0 && v2 >= 0) {
+            return 1;
+        } else if (v2 < 0 && v1 >= 0) {
+            return -1;
+        } else return 0;
     }
 
     @Override
--- src/test/java/it/saimao/shan_converter/word_sordings/ShanWordSortingTest.java
+++ src/test/java/it/saimao/shan_converter/word_sordings/ShanWordSortingTest.java
@@ -9,8 +9,7 @@ public class ShanWordSortingTest {
     @Test
     public void testConsonantSorting() {
         List<String> strings = Arrays.asList("ၵ", "ၵၶ", "တတ", "တၼ", "ၼ", "ပ", "ႁ", "ၶ", "လ");
-        strings.sort(new ShanWordSorting());
-        strings.forEach(System.out::println);
+        printData(strings);
     }
 
     @Test
@@ -20,15 +19,16 @@ public class ShanWordSortingTest {
     }
 
     private void printData(List<String> strings) {
+        strings.forEach(s -> System.out.print(s + " "));
         strings.sort(new ShanWordSorting());
-        strings.forEach(System.out::println);
+        System.out.println();
+        strings.forEach(s -> System.out.print(s + " "));
     }
 
     @Test
     public void testVowelSorting() {
         List<String> strings = Arrays.asList("ၵႃ", "ၵီ", "ၵူ", "ၵိုဝ်", "ၵိူဝ်", "ၵေ", "ၵူဝ်", "ၵႄ", "ၵေႃ");
-        strings.sort(new ShanWordSorting());
-        strings.forEach(System.out::println);
+        printData(strings);
 //        printData(Arrays.asList("ဢေႃ", "ဢူဝ်", "ဢူ"));
 
     }
@@ -56,20 +56,42 @@ public class ShanWordSortingTest {
         printData(Arrays.asList("ၶွတ်", "ၶၢတ်", "ၶျၢင်း"));
     }
 
+    // Bug
     @Test
     public void testReplaceKaikhin() {
         printData(Arrays.asList("ၵႆ", "ၵၢႆး", "ၶွႆ", "ၶွႆႈ", "ၶႆႇ"));
     }
 
+    // Bug
     @Test
     public void testDevowelizationWithTones() {
-//        printData(Arrays.asList("ၶၢတ်", "ၶၢတ်း", "ၶုတ်ႉ", "ၶုၼ်း", "ၶုၵ်ႉ", "ၶူၵ်ႉ", "ၶူၵ်", "ၶိူၵ်"));
-        printData(Arrays.asList("ၶၢတ်", "ၶုၵ်း"));
+        printData(Arrays.asList("ၶၢတ်", "ၶၢတ်း", "ၶုတ်ႉ", "ၶုၼ်း", "ၶုၵ်ႉ", "ၶူၵ်ႉ", "ၶူၵ်", "ၶိူၵ်"));
+//        printData(Arrays.asList("ၶၢတ်", "ၶုၵ်း"));
     }
 
     @Test
     public void testText1() {
-        printData(Arrays.asList("ၵႃႇလႃး", "ၵတ်ႉၶႅၼ်ႇ", "ၵျမ်းတေႉ", "ၵျေႃႇတေႉ", "ၵႃးပလိၵ်ႈ", "ၵႃးထႆး"));
+        printData(Arrays.asList("ၵႃႇလႃး", "ၵတ်ႉၶႅၼ်ႇ", "ၵႂ်ႊႁႅင်းဝႆႉ", "ၵြႃးပဵၼ်ၵလေး", "ၵျမ်းတေႉ", "ၵျေႃႇတေႉ", "ၵႃးပလိၵ်ႈ", "ၵႃးထႆး"));
+    }
+
+    @Test
+    public void testText2() {
+        printData(Arrays.asList("ႁူဝ်ၸႂ်", "ႁဵတ်ႉႁိူၼ်း", "ႁဵတ်ႉၵၢၼ်", "ပူႇမွၼ်ႇ", "ပူႇၼၢႆး", "ပူႇထဝ်ႈ", "ပူႇပႃ", "ပူၵရိတ်ႇ", "ပူႇသၽႃႇဝ", "ပူႇသၢမ်"));
+    }
+
+    @Test
+    public void testText3() {
+        printData(Arrays.asList("မၢၵ်ႇၵမ်ႇ", "မၢၵ်ႇၸွၵ်း", "မၢၵ်ႇၶိူဝ်", "မၢၵ်ႇၵႅမ်ႈၶွင်ႇ", "မၢၵ်ႇၵႅမ်ႈ", "မၢၵ်ႇမူင်ႊ", "မၢၵ်ႇမၼ်ႉ", "မၢၵ်ႇမွၼ်ႊ", "မၢၵ်ႇၶူဝ်", "မၢၵ်ႇလၢင်း"));
+    }
+
+    @Test
+    public void testText4() {
+        printData(Arrays.asList("ၸၢႆးမၢဝ်း", "ၸၢႆးၵွၼ်းၶမ်း", "ၸၢႆးၵွၼ်းမၢဝ်း", "ၸၢႆးၵွၼ်းလႅင်း", "ၸၢႆးၵွၼ်ႊလၢဝ်", "ၸၢႆးၵျေႃႇသူၺ်ႇ", "ၸၢႆးၵဵင်းတူင်", "ၸၢႆးၵဵင်းယၢင်း", "ၸၢႆးၵိုင်", "ၸၢႆးၵိူင်ႇ"));
+    }
+
+    @Test
+    public void testText5() {
+        printData(Arrays.asList("ၵၢၼ်ႁဵၼ်း", "ၵၢၼ်ႁဵတ်ႉ", "ၵၢၼ်မိူင်း", "ၵၢၼ်ၵႃႉၶၢႆ", "ၵၢၼ်တေႃႉၶူဝ်း", "ၵၢၼ်ဢွၼ်ႇလႅင်း", "ၵၢၼ်ပရႁိတ", "ၵၢၼ်လီၵုသူဝ်ႇ", "ၵၢၼ်ၸိူဝ်ႉၶိူဝ်း", "ၵၢၼ်လိၵ်ႈလၢႆး"));
     }
 
 
